33137c70bb182c21fb149d28be73db729ae3496a
Jean Maxo Cyprien <jmcyprien@solutions.ht>
2024-07-30 11:21:07 -0400

    Commit des taches

--- app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
@@ -1,15 +1,45 @@
 package fr.gobelins.dmi1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ComputeActivity : AppCompatActivity() {
+
+    private lateinit var btnCompute: Button
+    private lateinit var firstOperand: EditText
+    private lateinit var secondOperand: EditText
+    private lateinit var resultTextView: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.compute_activity)
 
         val operation = intent.getStringExtra("operation") ?: "ADD"
 
+        firstOperand = findViewById(R.id.first_operand)
+        secondOperand = findViewById(R.id.second_operand)
+        btnCompute = findViewById(R.id.btn_compute)
+        resultTextView = findViewById(R.id.result_text_view)
 
+        btnCompute.setOnClickListener {
+            val operand1 = firstOperand.text.toString().toDoubleOrNull()
+            val operand2 = secondOperand.text.toString().toDoubleOrNull()
+
+            if (operand1 != null && operand2 != null) {
+                val result = when (operation) {
+                    "ADD" -> operand1 + operand2
+                    else -> 0.0
+                }
+                resultTextView.text = "Result: $result"
+            } else {
+                resultTextView.text = "Please enter valid numbers"
+            }
+          }
+        }
     }
-}
--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -1,11 +1,62 @@
 package fr.gobelins.dmi1
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnHomeCompute: Button
+    private lateinit var btnSos: Button
+    private lateinit var btnSearch: Button
+    private lateinit var btnShare: Button
+    private lateinit var btnItineraire: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnHomeCompute = findViewById(R.id.btn_home_compute)
+        btnHomeCompute.setOnClickListener {
+            val intent = Intent(this, ComputeActivity::class.java)
+//            intent.extras?.putString("operation", "ADD")
+            startActivity(intent)
+        }
+
+        btnSos = findViewById(R.id.btn_sos)
+        btnSos.setOnClickListener{
+            val sosNumber = "112"
+            val callIntent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:$sosNumber")
+            }
+            startActivity(callIntent)
+        }
+
+        btnSearch = findViewById(R.id.btn_search)
+        btnSearch.setOnClickListener{
+            val searchIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://google.fr")
+            }
+            startActivity(searchIntent)
+        }
+
+        btnShare = findViewById(R.id.btn_share)
+        btnShare.setOnClickListener{
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Contenu partagé avec MBDS-HAITI.")
+                type = "text/plain"
+            }
+            val chooser = Intent.createChooser(shareIntent, "Partager via")
+            startActivity(chooser)
+        }
+
+        btnItineraire = findViewById(R.id.btn_itineraire)
+        btnItineraire.setOnClickListener{
+            val directionsIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=48.831201,2.356403")
+            }
+            startActivity(directionsIntent)
+        }
     }
 }
